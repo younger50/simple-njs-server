2410b51dbc0cb856ff3a31d626f222eedf925a47
younger50 <pt54088@gmail.com>
2015-08-22 14:59:46 +0800

    [Add] Add pure read method to server, add basic description to README

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,20 @@ var datatmp = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Data server', data: JSON.stringify(datatmp) });
 });
 
 router.get('/write', function(req ,res, next){
+	//write query string to global data array
 	qs = req.query
 	console.log(qs);
 	datatmp.push(qs);
+	res.send(datatmp);
+});
+
+router.get('/read', function(req ,res, next){
+	//response with global data array
+	console.log(datatmp);
 	res.send(datatmp);
 });
 
