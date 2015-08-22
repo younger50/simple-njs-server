aaa3da1c48a2d0396dcb47267638c0ae70b3ff5f
younger50 <pt54088@gmail.com>
2015-08-22 14:18:33 +0800

    [Test] Simple share data on server side runtime

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var datatmp = [];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/write', function(req ,res, next){
+	qs = req.query
+	console.log(qs);
+	datatmp.push(qs);
+	res.send(datatmp);
 });
 
 module.exports = router;
